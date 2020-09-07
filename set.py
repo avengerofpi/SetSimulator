81a4d840e9010cebf6a855fd86833773cefd8f56
Cory Washington <tbd>
2020-09-07 19:41:13 -0400

    messy, largely working - need to fix 'updateSetsFound'
    There's a lot of ugly debug logging going on, should clean that up.
    
    Need to define a valid version of the 'updateSetsFound' method.
    
    Need to use or delete or comment out the card interpretation dicts
      numbrDict, shapeDict, shadeDict, colorDict

--- set.py
+++ set.py
@@ -12,96 +12,127 @@ from time import time, sleep
 from collections import defaultdict
 from math import log10
 
-debug = True
-
 numbrDict = { 0: "One",     1: "Two",      2: "Three" }
 shapeDict = { 0: "Diamond", 1: "Squiggle", 2: "Oval"}
 shadeDict = { 0: "Solid",   1: "Stripe",   2: "Open" }
 colorDict = { 0: "Red",     1: "Green",    2: "Color-blind Blue" }
 
-dealSize_init = 12
-dealSize_add = 3
-dealMoreCardsThreshold = dealSize_init
-
+debug = False
 def debugPrint(args):
     if debug:
         print(args)
-        sleep(0.01)
+        sleep(0.025)
 
-def newShuffledDeck():
-    debugPrint("shuffing a new deck")
-    deck = [card for card in product(range(3), range(3), range(3), range(3))]
-    shuffle(deck)
-    return deck
+class Set:
+    DEAL_SIZE_INIT = 12
+    DEAL_SIZE_ADD = 3
+    DEAL_MORE_CARDS_THRESHOLD = DEAL_SIZE_INIT
 
-# The return of the function is for loop control later on. The global variable is used directly.
-setsFound = None
-def updateSetsFound(hand):
-    setsFound = list()
-    setsFound = (hand[:3])
+    def debugPrint(self, s):
+        debugPrint(s)
 
-    debugPrint("looking for new sets")
-    debugPrint("  found sets: {}".format(setsFound))
+    def debugPrintDetails(self, s = ""):
+        s and self.debugPrint(s)
+        debugPrint("  Hand size / Deck size: {0:>2d} / {1:2>d}".format(len(self.hand), len(self.mainDeck)))
+        debugPrint("  Current Hand: {}".format(self.hand))
 
-    return setsFound
+    def __init__(self):
+        self.deck = [card for card in product(range(3), range(3), range(3), range(3))]
+        self.mainDeck = None
+        self.hand = None
+        self.setsFound = None
 
-def removeRandomSetFromHand(hand):
-    # 'hand' is updated in-place, no return necessary
-    # meh, just grab the first set found
-    set = setsFound[0]
-    debugPrint("removing a set that was found: {}".format(set))
-    for card in set:
-        hand.remove(card)
+    def shuffle(self):
+        # Shuffle the deck
+        debugPrint("Shuffing the deck")
+        shuffle(self.deck)
 
-# Give the player another batch of cards.
-def dealMoreCards(hand, deck):
-    newCards = deck[0:dealSize_add]
-    hand.append(newCards)
-    deck = deck[dealSize_add:]
-    debugPrint("dealing more cards: {}".format(newCards))
-    return hand, deck
+    def resetGame(self):
+        self.debugPrint("Resetting the whole game (deck, mainDeck, hand, setsFound)")
+        self.shuffle()
+        self.mainDeck = None
+        self.hand = None
+        self.setsFound = None
 
-# Play a single round or Set, starting with a new shuffled deck.
-# Include a timer mechanism to kill the round if it's taking too  long.
-maxPlayDuration = 10 # seconds
-def playOneRound():
-    debugPrint("playing a new round")
-    # Return number of cards left in play at the end
-    deck = newShuffledDeck()
-    hand = deck[0:dealSize_init]
-    deck = deck[dealSize_init:]
+    def dealFirstHand(self):
+        self.hand = self.mainDeck[0:self.DEAL_SIZE_INIT]
+        self.mainDeck = self.mainDeck[self.DEAL_SIZE_INIT:]
+        self.debugPrintDetails("First hand has been dealt:")
 
-    debugPrint("new hand: {}".format(hand))
+    def dealMore(self):
+        self.hand.extend(self.mainDeck[0:self.DEAL_SIZE_ADD])
+        self.mainDeck = self.mainDeck[self.DEAL_SIZE_ADD:]
+        self.debugPrintDetails("Dealt {} more cards to hand:".format(self.DEAL_SIZE_ADD))
 
-    # Play until no more deck and no more sets can be found
-    # Perform the updateSetsFound() function first to ensure it is always run (beware shortcircuiting!)
-    startTime = time()
-    maxEndTime = startTime + (maxPlayDuration * 1000)
-    while updateSetsFound(hand) or (len(deck) == 0):
-        if time() > maxEndTime:
-            raise RuntimeError("The current round is taking too long - max runtime is {} seconds".format(maxPlayDuration))
-        if setsFound:
-            debugPrint("{} sets found, proceed to remove one...".format(len(setsFound)))
-            removeRandomSetFromHand(hand)
-        else:
-            debugPrint("NO sets found...")
-            if len(deck) == 0:
-                debugPrint("we are done")
-                # No sets and no more cards to deal. This round is done.
-                break
-        if (not setsFound) or (len(hand) < dealMoreCardsThreshold):
-            hand, deck = dealMoreCards(hand, deck)
-    debugPrint(hand)
-    return len(hand)
+    def startNewRound(self):
+        self.debugPrint("Starting new round")
+        self.resetGame()
+        self.mainDeck = self.deck
+        self.dealFirstHand()
+
+    def removeRandomSetFromHand(self):
+        # meh, just grab the first set found
+        setToRemove = self.setsFound[0]
+        self.debugPrint("Removing a set that was found: {}".format(setToRemove))
+        for card in setToRemove:
+            self.debugPrint(" Removing card '{}'".format(card))
+            self.hand.remove(card)
+        self.debugPrintDetails()
+
+    def updateSetsFound(self):
+        self.setsFound = list()
+
+        # !!!!! FAKE SET FINDING CODE - FILL IN REAL LOGIC LATER !!!!!
+        self.setsFound = (self.hand[:3],)
+
+        self.debugPrint("Looking for new sets")
+        self.debugPrint("  Found sets: {}".format(self.setsFound))
+        self.debugPrintDetails()
+
+        return self.setsFound
 
 
-iters = 1
+    # Play a single round or Set, starting with a new shuffled deck.
+    # Include a timer mechanism to kill the round if it's taking too  long.
+    MAX_PLAY_DURATION = 20 # seconds
+    def playOneRound(self):
+        self.debugPrint("Playing a new round")
+        self.startNewRound()
+
+        # Play until no more deck and no more sets can be found
+        # Perform the updateSetsFound() function first to ensure it is always run (beware shortcircuiting!)
+        startTime = time()
+        maxEndTime = startTime + self.MAX_PLAY_DURATION
+        self.debugPrint("\n\nstartTime: {}\nendTime:   {}\n\n".format(startTime, maxEndTime))
+        while (len(self.hand) and self.updateSetsFound()) or (len(self.mainDeck) > 0):
+            currentTime = time()
+            secondsLeft = maxEndTime - currentTime
+            self.debugPrint("\n\ncurrentTime: {}\nendTime:     {}\nsecondsLeft: {}\n\n".format(currentTime, maxEndTime, secondsLeft))
+            if time() > maxEndTime:
+                raise RuntimeError("The current round is taking too long - max runtime is {} seconds".format(self.MAX_PLAY_DURATION))
+            if self.setsFound:
+                self.debugPrint("{} sets found, proceed to remove one...".format(len(self.setsFound)))
+                self.removeRandomSetFromHand()
+            else:
+                self.debugPrint("NO sets found...")
+                if len(self.mainDeck) == 0:
+                    self.debugPrint("we are done")
+                    # No sets and no more cards to deal. This round is done.
+                    break
+            if (not self.setsFound) or (len(self.hand) < self.DEAL_MORE_CARDS_THRESHOLD ):
+                self.dealMore()
+        self.debugPrint(self.hand)
+        return self.hand
+
+iters = 10
 freqDigits = int(log10(iters))
 freqDict = defaultdict(int)
+set = Set()
 for i in range(iters):
     debugPrint("beginning iteration {0:>{1}d}".format(i, freqDigits))
-    freqDict[playOneRound()] += 1
+    set.playOneRound()
+    freqDict[len(set.hand)] += 1
 
 for (size, freq) in sorted(freqDict.items()):
-    debugPrint("Size: {0:>2d}  Freq: {1:>{2}d}".format(size, freq, freqDigits))
+    print("Size: {0:>2d}  Freq: {1:>{2}d}".format(size, freq, freqDigits))
 
